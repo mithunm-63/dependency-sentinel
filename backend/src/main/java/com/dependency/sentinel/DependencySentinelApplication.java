ffa3e7ca34efe13067db2382c7bfd358529917d8
mithunm-63 <mithun063.m@gmail.com>
2026-09-05 12:46:35 +0530

    feat: add Spring Boot application

---
+++ backend/src/main/java/com/dependency/sentinel/DependencySentinelApplication.java
@@ -0,0 +1,11 @@
+package com.dependency.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencySentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DependencySentinelApplication.class, args);
+    }
+}
