5ca37b69e0856ebfa2cfed3704d9cbb80eb0c69e
mithunm-63 <mithun063.m@gmail.com>
2026-09-05 12:44:09 +0530

    feat: add Dependency Sentinel backend

---
+++ backend/src/main/java/com/dependency/sentinel/project/ProjectRepository.java
@@ -0,0 +1,5 @@
+package com.dependency.sentinel.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {}
