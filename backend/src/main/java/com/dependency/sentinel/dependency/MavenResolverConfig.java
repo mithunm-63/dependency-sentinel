1cda74c3d265e03204c12f5f3c04b01d5b29805f
mithunm-63 <mithun063.m@gmail.com>
2026-09-06 13:09:45 +0530

    refactor: create Maven Resolver sessions per scan

--- backend/src/main/java/com/dependency/sentinel/dependency/MavenResolverConfig.java
+++ backend/src/main/java/com/dependency/sentinel/dependency/MavenResolverConfig.java
@@ -1,15 +1,10 @@
 package com.dependency.sentinel.dependency;
 
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.RepositorySystemSession;
-import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.supplier.RepositorySystemSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.File;
 
 @Configuration
 public class MavenResolverConfig {
@@ -17,15 +12,6 @@ public class MavenResolverConfig {
     @Bean(destroyMethod = "shutdown")
     public RepositorySystem repositorySystem() {
         return new RepositorySystemSupplier().get();
-    }
-
-    @Bean
-    public RepositorySystemSession repositorySystemSession(RepositorySystem repositorySystem) {
-        var session = MavenRepositorySystemUtils.newSession();
-        File localRepositoryDir = new File(System.getProperty("java.io.tmpdir"), "dependency-sentinel-m2");
-        LocalRepository localRepository = new LocalRepository(localRepositoryDir);
-        session.setLocalRepositoryManager(repositorySystem.newLocalRepositoryManager(session, localRepository));
-        return session;
     }
 
     @Bean
